5c09f43efd8e35c3d951a080045e529bfa2cb730
jfranco <juandavid.francob@gmail.com>
2026-02-20 23:45:34 -0500

    feat(sidebar): Add admin checks and improve sidebar layout

--- mercadoai-frontend/src/app/shared/layout/sidebar/sidebar.component.ts
+++ mercadoai-frontend/src/app/shared/layout/sidebar/sidebar.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './sidebar.component.html',
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
+  isAdmin: boolean = false;
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+    console.log(this.authService.isAdmin());
+    this.isAdmin = this.authService.isAdmin();
+    console.log('isAdmin:', this.isAdmin);
+  }
 
 }
